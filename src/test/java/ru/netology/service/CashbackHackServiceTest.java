9b3bcc3dc1518e8e0e3fe2c2530369c72572e252
Mikhail Chebaev <make7650@rambler.ru>
2020-04-22 21:07:25 +0300

    create junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,14 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    void enterIntegerValue () {
+
+    }
+
+}
