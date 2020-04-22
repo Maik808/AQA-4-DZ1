940a29f9b506cbcc94958737f47a30b2e3aa6822
Mikhail Chebaev <make7650@rambler.ru>
2020-04-22 21:21:40 +0300

    junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,8 +7,39 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    void enterIntegerValue () {
-
+    public void enterIntegerValue() {
+        final CashbackHackService cashback = new CashbackHackService();
+        int amount = 5700;
+        int actual = cashback.remain(amount);
+        int expected = 300;
+        assertEquals(expected, actual);
     }
 
+    @Test
+    public void enterNullValue() {
+        final CashbackHackService cashback = new CashbackHackService();
+        int amount = 0;
+        int actual = cashback.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void enterRoundValue() {
+        final CashbackHackService cashback = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashback.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void enterNegativeValue() {
+        final CashbackHackService cashback = new CashbackHackService();
+        int amount = -50000;
+        int actual = cashback.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
 }
+
